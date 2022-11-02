5f8ef36c4c0fa10339883f6931bfa6e51f0ba2ad
LeonKar <l.karagishev@gmail.com>
2022-11-02 18:39:33 +0100

    Entrega main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,22 @@
 package application;
 	
+import control.controlador;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+			controlador control = new controlador();
+			loader.setController(control);
+			Parent root = loader.load();
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
