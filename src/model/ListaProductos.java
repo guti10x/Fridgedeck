9fa643a44b2346f94235218b0528d6be5b50ba3e
LeonKar <l.karagishev@gmail.com>
2023-01-24 21:14:43 +0100

    Add files via upload

---
+++ src/model/ListaProductos.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.List;
+
+public class ListaProductos {
+	public int id_user;
+	public List<Productos> lista_productos;
+	
+	public ListaProductos(int id_user, List<Productos> lista_productos) {
+		super();
+		this.id_user = id_user;
+		this.lista_productos = lista_productos;
+	}   
+}
