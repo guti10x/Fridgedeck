a6c981f72c7e45f9795426c347ce7a7f8320011c
LeonKar <l.karagishev@gmail.com>
2023-01-27 20:17:06 +0100

    Profile update

--- src/control/controladorVentanaInformacionUsuario.java
+++ src/control/controladorVentanaInformacionUsuario.java
@@ -1,12 +1,27 @@
 package control;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import application.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.FridgeDate;
+import model.ListaCompras;
+import model.ListaProductos;
 
 public class controladorVentanaInformacionUsuario {
 
@@ -50,6 +65,39 @@ public class controladorVentanaInformacionUsuario {
           assert tfpasswordInfo != null : "fx:id=\"tfpasswordInfo\" was not injected: check your FXML file 'ventana_Informacion_usuario.fxml'.";
           assert tfNameSurnameInfo != null : "fx:id=\"tfNameSurnameInfo\" was not injected: check your FXML file 'ventana_Informacion_usuario.fxml'.";
           assert tfFridgeaddressInfo != null : "fx:id=\"tfFridgeaddressInfo\" was not injected: check your FXML file 'ventana_Informacion_usuario.fxml'.";
-
+          
+          int user_id = 0;
+  		try {
+  		      File myObj = new File("user_id.txt");
+  		      Scanner myReader = new Scanner(myObj);
+  		      while (myReader.hasNextLine()) {
+  		        String id = myReader.nextLine();
+  		        user_id = Integer.valueOf(id);
+  		      }
+  		      myReader.close();
+  		    } catch (FileNotFoundException e) {
+  		      System.out.println("An error occurred.");
+  		      e.printStackTrace();
+  		    }
+  		
+  			Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get("userbase.json"));
+			Usuario[] users = new Gson().fromJson(reader, Usuario[].class);
+			for(int i=0; i<users.length; i++) {
+				if(users[i].id==user_id) {
+					tfUsernameInfo.setText(String.valueOf(users[user_id].username));
+					tfemailinfo.setText(String.valueOf(users[user_id].email));
+					tfpasswordInfo.setText(String.valueOf(users[user_id].password));
+					tfNameSurnameInfo.setText(String.valueOf(users[user_id].name_surname));
+					tfFridgeaddressInfo.setText(String.valueOf(users[user_id].fridge_adress));
+				}
+			}
+			reader.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     }
 }
